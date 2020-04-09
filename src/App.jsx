087da4b9f5943fa798cfa4f6a11ab7a3c7c71270
dernathul <lautaroparracab@gmail.com>
2020-04-09 08:14:52 +0200

    finishing the basic scaffold and first lines of code in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+ const App = () => {
+  return (
+    <div>
+      <h1> Cuadramiga </h1>
+      <h2> tu red de comercio justo </h2>
+    </div>
+  )
+}
+
+
+export default App
+
+
